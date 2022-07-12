ef5e773943087aaef342a7cb1e1f95d58b56b66b
nkilm <nikhilmohitelhs@gmail.com>
2022-07-12 14:38:54 +0530

    navbar jsx done

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import {images} from "../../constants"
+import { images } from "../../constants"
 import "./Navbar.scss"
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div>
-            <img src={images.logo} alt="logo" />
-        </div>
-    </nav>
-  )
+    return (
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <img src={images.logo} alt="logo" />
+            </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div>
+                            <a href={`#${item}`}>{item}</a>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
