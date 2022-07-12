14b0a23408f8ef5df2f0931b887ffede3afb897d
nkilm <nikhilmohitelhs@gmail.com>
2022-07-12 12:49:46 +0530

    containers: starter files added

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import { About, Footer, Header, Skills, Testimonial, Work } from "./containers"
+
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
     </div>
   );
 }
