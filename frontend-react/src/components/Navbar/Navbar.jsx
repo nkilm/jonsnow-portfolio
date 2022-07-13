0894b3a819fac6c2c200c3e2073966d0cbf5367e
nkilm <nikhilmohitelhs@gmail.com>
2022-07-13 12:52:20 +0530

    navbar responsive css done

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from "../../constants"
+import { HiMenuAlt3, HiX } from "react-icons/hi"
+import { motion } from "framer-motion";
 import "./Navbar.scss"
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <nav className='app__navbar'>
             <div className='app__navbar-logo'>
@@ -11,12 +15,33 @@ const Navbar = () => {
             <ul className='app__navbar-links'>
                 {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
                     <li className='app__flex p-text' key={`link-${item}`}>
-                        <div /> 
+                        <div />
                         {/* The above div is used to create that dot when hovered */}
                         <a href={`#${item}`}>{item}</a>
                     </li>
                 ))}
             </ul>
+
+            <div className="app__navbar-menu">
+                <HiMenuAlt3 onClick={() => setToggle(true)} />
+
+                {/* if toggle is true  */}
+                {toggle &&
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                }
+            </div>
         </nav>
     )
 }
