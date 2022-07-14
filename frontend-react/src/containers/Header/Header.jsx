a6fe7a35eb972f20e6e98d974980d8078922bcee
nkilm <nikhilmohitelhs@gmail.com>
2022-07-14 11:53:01 +0530

    header jsx done

--- frontend-react/src/containers/Header/Header.jsx
+++ frontend-react/src/containers/Header/Header.jsx
@@ -1,10 +1,70 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import "./Header.scss"
+import { images } from "../../constants/index"
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div id='home' className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span></span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am </p>
+              <h1 className="head-text">Micael</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">ML</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ x: [-100, 0] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={Image.profile} alt="profile_bg" />
+
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) =>
+          (<div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>)
+        )}
+      </motion.div>
+    </div>
   )
 }
 
