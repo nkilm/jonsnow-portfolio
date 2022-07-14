ef963a78d5e02c1a83fa1ac1ccf75d1a612e4f13
nkilm <nikhilmohitelhs@gmail.com>
2022-07-14 14:30:31 +0530

    setup a connection to sanity.io db

---
+++ frontend-react/src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = sanityClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: "production",
+    apiVersion: "2022-07-01",
+    useCdn: true,
+    token: process.env.SANITY_PROJECT_API_TOKEN
+})
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
