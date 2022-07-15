65d3c5e449a4b4e6f37164afd0f6c9d780edd45a
nkilm <nikhilmohitelhs@gmail.com>
2022-07-15 13:07:21 +0530

    load content from backend

--- frontend-react/src/containers/About/About.jsx
+++ frontend-react/src/containers/About/About.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from "framer-motion"
 import "./About.scss"
-import { images } from "../../constants"
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: "Web Development", description: "I am a good web developer", imgUrl: images.about01 },
-  { title: "Web Design", description: "I am a good web developer", imgUrl: images.about02 },
-  { title: "UI/UX", description: "I am a good web developer", imgUrl: images.about03 },
-  { title: "MERN Stack", description: "I am a good web developer", imgUrl: images.about04 }
-]
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    // set up a query
+    const query = '*[_type == "abouts"]';
+
+    // fetch from db 
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, [])
+
   return (
     <>
       <h2 className="head-text">
@@ -26,7 +33,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
