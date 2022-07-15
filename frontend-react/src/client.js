ae46b857227b7e320a6f2137b65123a5de248bf0
nkilm <nikhilmohitelhs@gmail.com>
2022-07-15 13:07:59 +0530

    store ID and token in .env.local

--- frontend-react/src/client.js
+++ frontend-react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId: process.env.SANITY_PROJECT_ID,
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: "production",
     apiVersion: "2022-07-01",
     useCdn: true,
-    token: process.env.SANITY_PROJECT_API_TOKEN
+    token: process.env.REACT_APP_SANITY_PROJECT_API_TOKEN
 })
 
 const builder = imageUrlBuilder(client);
