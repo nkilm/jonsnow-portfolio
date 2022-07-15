e2c91aa8fa6bc355def505e0f0bf98fab90df253
nkilm <nikhilmohitelhs@gmail.com>
2022-07-15 13:54:40 +0530

    Higher order components

---
+++ frontend-react/src/wrapper/AppWrap.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavigationDots, SocialMedia } from "../components"
+
+// HOC - Higher Order Component
+
+const AppWrap = (Component,idName,classNames) => function HOC(){
+    return (
+        <div id={idName} className={`app__container ${classNames}`}>
+            <SocialMedia/>
+
+            <div className="app__wrapper app__flex">
+                <Component/>
+            </div>
+
+            <div className="copyrights">
+                <p className="p-text">@2022 Jon Snow</p>
+                <p className="p-text">All Rights Reserved</p>
+            </div>
+
+            <NavigationDots active={idName}/>
+        </div>
+    )
+}
+
+export default AppWrap
