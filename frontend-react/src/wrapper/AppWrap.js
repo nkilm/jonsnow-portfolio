4c2da6253434f654258dbb804e36f702b0631b51
nkilm <nikhilmohitelhs@gmail.com>
2022-07-27 00:07:10 +0530

    update wrappers

--- frontend-react/src/wrapper/AppWrap.js
+++ frontend-react/src/wrapper/AppWrap.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import { NavigationDots, SocialMedia } from "../components"
+import React from 'react';
+import { NavigationDots, SocialMedia } from '../components';
 
-// HOC - Higher Order Component
+const AppWrap = (Component, idName, classNames) => function HOC() {
+  return (
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
+      <div className="app__wrapper app__flex">
+        <Component />
 
-const AppWrap = (Component,idName,classNames) => function HOC(){
-    return (
-        <div id={idName} className={`app__container ${classNames}`}>
-            <SocialMedia/>
-
-            <div className="app__wrapper app__flex">
-                <Component/>
-            </div>
-
-            <div className="copyrights">
-                <p className="p-text">@2022 Jon Snow</p>
-                <p className="p-text">All Rights Reserved</p>
-            </div>
-
-            <NavigationDots active={idName}/>
+        <div className="copyright">
+          <p className="p-text">@2022 Jon Snow</p>
+          <p className="p-text">All rights reserved</p>
         </div>
-    )
-}
+      </div>
+      <NavigationDots active={idName} />
+    </div>
+  );
+};
 
 export default AppWrap;
--- frontend-react/src/wrapper/index.js
+++ frontend-react/src/wrapper/index.js
@@ -1,2 +1,7 @@
-export {default as AppWrap} from "./AppWrap"
-export {default as MotionWrap} from "./MotionWrap"
+import AppWrap from './AppWrap';
+import MotionWrap from './MotionWrap';
+
+export {
+  AppWrap,
+  MotionWrap,
+};
